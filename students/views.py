ccd13ab93bcf7114b6978caba113059f5a7e1edd
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-08-28 18:19:26 +0300

    Add templates

--- students/views.py
+++ students/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views for Students
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+def students_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+# Views for Groups
+
+def groups_list(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,32 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
+    # Students urls
+    url(r'^$', 'students.views.students_list', name='home'),
+    url(r'^students/add/$', 'students.views.students_add',
+        name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$',
+        'students.views.students_edit',
+        name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete',
+        name='students_delete'),
+
+    # Groups urls
+
+    url(r'^groups/$', 'students.views.groups_list',
+        name='groups'),
+    url(r'^groups/add/$', 'students.views.groups_add',
+        name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', 'students.views.groups_edit',
+        name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$',
+        'students.views.groups_delete',
+        name='groups_delete'),
+
     url(r'^admin/', include(admin.site.urls)),
-]
+)
