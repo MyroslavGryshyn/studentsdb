eafecb5c3527282a569dc5435a8e5b1f450caa67
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-09-01 19:05:40 +0300

    Add static files + transformed template into view mode

--- students/views.py
+++ students/views.py
@@ -6,6 +6,27 @@ from django.http import HttpResponse
 def students_list(request):
     return render(request, 'students/students_list.html', {})
 
+def students_list(request):
+    students = (
+        {'id': 1,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/podoba3.jpg'},
+        {'id': 2,
+         'first_name': u'Корост',
+         'last_name': u'Андрій',
+         'ticket': 2123,
+         'image': 'img/podoba3.jpg'},
+        {'id': 3,
+         'first_name': u'Antony',
+         'last_name': u'Pad',
+         'ticket': 2124,
+         'image': 'img/podoba3.jpg'},
+    )
+    return render(request, 'students/students_list.html',
+                  {'students': students})
+
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
 
