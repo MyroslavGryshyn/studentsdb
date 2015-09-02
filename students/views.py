352f2ae58822f339a5397f0dc6ce57ada7b3a037
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-09-02 16:23:51 +0300

    Fixed bug with Ukrainian symbols

--- students/views.py
+++ students/views.py
@@ -1,8 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
-
 def students_list(request):
     return render(request, 'students/students_list.html', {})
 
