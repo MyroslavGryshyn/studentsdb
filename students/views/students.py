a0e3999c6f04a3cc5f051fc5a452c64b083dea6a
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-09-15 18:56:23 +0300

    Improved image show

--- students/views/students.py
+++ students/views/students.py
@@ -2,29 +2,12 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from ..models import Student
 
 # Views for Students
-def students_list(request):
-    return render(request, 'students/students_list.html', {})
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Віталій',
-         'last_name': u'Подоба',
-         'ticket': 235,
-         'image': 'img/podoba3.jpg'},
-        {'id': 2,
-         'first_name': u'Корост',
-         'last_name': u'Андрій',
-         'ticket': 2123,
-         'image': 'img/podoba3.jpg'},
-        {'id': 3,
-         'first_name': u'Antony',
-         'last_name': u'Pad',
-         'ticket': 2124,
-         'image': 'img/podoba3.jpg'},
-    )
+    students = Student.objects.all()
     return render(request, 'students/students_list.html',
                   {'students': students})
 
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
+from .settings import MEDIA_ROOT, DEBUG
+
 urlpatterns = patterns('',
     # Students urls
     url(r'^$', 'students.views.students.students_list', name='home'),
@@ -47,3 +49,9 @@ urlpatterns = patterns('',
     
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if DEBUG:
+    # serve files from media folder
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': MEDIA_ROOT}))
