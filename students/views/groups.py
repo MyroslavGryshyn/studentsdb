7a7f147a3e00dfcf55995e364a779eee07ca9051
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-09-28 18:19:42 +0300

    Registered Group model in admin.py Added view for groups_page + changed template.

--- students/views/groups.py
+++ students/views/groups.py
@@ -1,22 +1,36 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from ..models import Group
 
 # Views for Groups
 
 def groups_list(request):
-    groups = (
-        {'id': 1,
-         'name': u'Віталій',
-         'leader': u'Подоба'},
-        {'id': 2,
-         'name': u'Корост',
-         'leader': u'Андрій'},
-        {'id': 3,
-         'name': u'Antony',
-         'leader': u'Pad'},
-    )
-    return render(request, 'students/groups_list.html',
+    groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    # paginate groups
+    numb_pages = 3
+
+    paginator = Paginator(groups, numb_pages)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        groups = paginator.page(paginator.num_pages)
+
+    return render(request, 'students/groups_list.html', 
                   {'groups': groups})
 
 def groups_add(request):
--- students/views/students.py
+++ students/views/students.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models import Student
 
 # Views for Students
@@ -9,39 +10,27 @@ from ..models import Student
 def students_list(request):
     students = Student.objects.all()
 
-    # Ordering students list
+    # try to order students list
     order_by = request.GET.get('order_by', '')
-
-    if order_by in ('last_name', 'first_name', 'ticket', 'id'):
+    if order_by in ('last_name', 'first_name', 'ticket'):
         students = students.order_by(order_by)
-
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
 
-    #page - from template
+    # paginate students
+    paginator = Paginator(students, 3)
     page = request.GET.get('page')
-    # If page is not an integer, deliver first page.
     try:
-        page = int(page)
-    except (ValueError, TypeError):
-        page = 1
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        students = paginator.page(paginator.num_pages)
 
-    per_page = 2
-
-    num_pages, remainder = divmod(len(students), per_page)
-    number_of_pages = num_pages + 1 if remainder else num_pages
-
-    if page < 1:
-        page = 1
-    elif page > number_of_pages and number_of_pages:
-        page = number_of_pages
-
-    students = students[((int(page)-1)*per_page):((int(page)-1)*per_page+per_page)]
-                        
-    return render(request, 'students/students_list.html', \
-                  {'students': students,  \
-                   'pages': [x for x in xrange(1, number_of_pages+1)], \
-                   'page': page})
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
