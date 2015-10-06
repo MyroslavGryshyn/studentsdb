4d2f6997a291f2e601e49a34e4a221df5b24647e
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-10-06 10:35:53 +0300

    Splited models.py to studen, group and added monthjournal model

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Student, Group
+from .models.student import Student
+from .models.group import Group
+from .models.monthjournal import MonthJournal
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Group)
+admin.site.register(MonthJournal)
---
+++ students/models/monthjournal.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+class MonthJournal(models.Model):
+    """Student monthly journal"""
+
+    class Meta(object):
+        verbose_name = u"Місячний журнал"
+        verbose_name_plural = u"Місячний журнал"
+
+    student = models.ForeignKey('Student',
+        verbose_name=u"Студент",
+        blank=False,
+        unique_for_month='date')
+
+    student = models.ForeignKey('Student',
+        verbose_name=u'Студент',
+        blank=False,
+        unique_for_month='date')
+
+    # we only need year and month, so always set day to first day of the month
+    date = models.DateField(
+        verbose_name=u'Дата',
+        blank=False)
+
+    # list of days, each says whether student was present or not
+    present_day1 = models.BooleanField(default=False)
+    present_day2 = models.BooleanField(default=False)
+    present_day3 = models.BooleanField(default=False)
+    present_day4 = models.BooleanField(default=False)
+    present_day5 = models.BooleanField(default=False)
+    present_day6 = models.BooleanField(default=False)
+    present_day7 = models.BooleanField(default=False)
+    present_day8 = models.BooleanField(default=False)
+    present_day9 = models.BooleanField(default=False)
+    present_day10 = models.BooleanField(default=False)
+    present_day11 = models.BooleanField(default=False)
+    present_day12 = models.BooleanField(default=False)
+    present_day13 = models.BooleanField(default=False)
+    present_day14 = models.BooleanField(default=False)
+    present_day15 = models.BooleanField(default=False)
+    present_day16 = models.BooleanField(default=False)
+    present_day17 = models.BooleanField(default=False)
+    present_day18 = models.BooleanField(default=False)
+    present_day19 = models.BooleanField(default=False)
+    present_day20 = models.BooleanField(default=False)
+    present_day21 = models.BooleanField(default=False)
+    present_day22 = models.BooleanField(default=False)
+    present_day23 = models.BooleanField(default=False)
+    present_day24 = models.BooleanField(default=False)
+    present_day25 = models.BooleanField(default=False)
+    present_day26 = models.BooleanField(default=False)
+    present_day27 = models.BooleanField(default=False)
+    present_day28 = models.BooleanField(default=False)
+    present_day29 = models.BooleanField(default=False)
+    present_day30 = models.BooleanField(default=False)
+    present_day31 = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return u'%s: %d, %d' % (self.student.last_name, self.date.month,
+                                self.date.year)
