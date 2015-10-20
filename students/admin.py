809e89a14b2623fc90879e8bd0b426a36ae1d9de
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-10-20 19:45:13 +0300

    Customized admin forms for Students and Groups

--- students/admin.py
+++ students/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
-from .models.student import Student
-from .models.group import Group
+from .models import Student, Group
 from .models.monthjournal import MonthJournal
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 
+                     'middle_name', 'ticket', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader', 'notes']
+    list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_per_page = 10
+    search_fields = ['title', 'leader', 
+                     'notes']
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'pk': obj.id})
+
+
 # Register your models here.
-admin.site.register(Student)
-admin.site.register(Group)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(MonthJournal)
