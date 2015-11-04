bdc321da12a2d9389abb8995157f631480b8a7be
MyroslavGryshyn <sergiy.1911@gmail.com>
2015-11-04 15:45:34 +0200

    Disabled Esc in form modal window

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -58,10 +58,91 @@ function initDateFields() {
   });
 }
 
+function initEditStudentPage() {
+  $('a.student-edit-form-link').click(function(event){
+    var link = $(this);
+    $.ajax({
+      'url': link.attr('href'),
+      'dataType': 'html',
+      'type': 'get',
+      'success': function(data, status, xhr){
+        // check if we got successfull response from the server
+
+        if (status != 'success') {
+          alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+          return false;
+        }
+
+        // update modal window with arrived content from the server
+        var modal = $('#myModal'),
+          html = $(data), form = html.find('#content-column form');
+
+        modal.find('.modal-title').html(html.find('#content-column h2').text());
+        modal.find('.modal-body').html(form);
+
+        // init our edit form
+        initEditStudentForm(form, modal);
+
+        // setup and show modal window finally
+        modal.modal({
+        'keyboard': false,
+        'backdrop': false,
+        'show': true
+        });
+      },
+      'error': function(){
+        alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+        return false;
+      }
+    });
+    return false;
+  });
+}
+
+function initEditStudentForm(form, modal) {
+  // attach datepicker
+  initDateFields();
+
+  // close modal window on Cancel button click
+  form.find('input[name="cancel_button"]').click(function(event){
+    modal.modal('hide');
+    return false;
+  });
+
+  // make form work in AJAX mode
+  form.ajaxForm({
+    'dataType': 'html',
+    'error': function(){
+        alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+        return false;
+    },
+    'success': function(data, status, xhr) {
+      var html = $(data), newform = html.find('#content-column form');
+
+      // copy alert to modal window
+      modal.find('.modal-body').html(html.find('.alert'));
+
+      // copy form to modal if we found it in server response
+      if (newform.length > 0) {
+        modal.find('.modal-body').append(newform);
+
+        // initialize form fields and buttons
+        initEditStudentForm(newform, modal);
+      } else {
+        // if no form, it means success and we need to reload page
+        // to get updated students list;
+        // reload after 2 seconds, so that user can read success message
+        setTimeout(function(){location.reload(true);}, 500);
+      }
+    }
+  });
+}
+
 $(document).ready(function(){
   initJournal();
   initGroupSelector();
   initDateFields();
+  initEditStudentPage();
 });
 
 
